88aadefbc36d7ed54015f2bccb9fe3fd78aff372
CMTuz <83449012+CMTuz@users.noreply.github.com>
2021-05-07 13:27:19 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var a = prompt('Введите 1 число'); 
+var b = prompt('Введите 2 число'); 
+var c = prompt('Введите 3 число'); 
+
+if(a<=b && a>=c || a>=b && a <=c ){
+    alert('среднее число ' +a);
+}else if (b <= a && b >= c || b >= a && b <= c){
+    alert('среднее число' +b);
+}else if(c <= a && c >= b || c >= a && c <= b){
+    alert('среднее число ' + с);
+}
+
+
+
+
